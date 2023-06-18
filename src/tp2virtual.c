1156b6f8ad1af00fdff213682a34937f46dddeec
Gabriel Castelo Branco <gcastelo.rocha@gmail.com>
2023-06-18 12:16:38 -0300

    Co-authored-by: HenriqueRotsen <HenriqueRotsen@users.noreply.github.com>

--- src/tp2virtual.c
+++ src/tp2virtual.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 struct Page
 {
@@ -8,6 +9,7 @@ struct Page
     int dirty;
     int second_chance;
     int timer;
+    int ref_bit;
 };
 
 struct Recorder
@@ -22,8 +24,8 @@ struct Recorder rec;
 void print_results()
 {
     printf("Executando o simulador... \n\tTécnica de reposição: %s\n\tArquivo de entrada: %s\n\tTamanho das Páginas: %sKB\n\tTamanho da memória usada: %sKB\n", rec.configuracao[0], rec.configuracao[1], rec.configuracao[2], rec.configuracao[3]);
-    printf("\tPage faults: %u\n", rec.page_faults);
-    printf("\tPáginas sujas escritas: %u", rec.dirty_writes);
+    printf("\tPage faults: %d\n", rec.page_faults);
+    printf("\tPáginas sujas escritas: %d", rec.dirty_writes);
 }
 
 int size_calculator(int page_size)
@@ -46,6 +48,7 @@ void add(struct Page *page_table, int idx, unsigned addr, int timer)
 {
     page_table[idx].id = addr;
     page_table[idx].timer = timer;
+    page_table[idx].dirty = 1;
 }
 
 int find(struct Page *page_table, int size, unsigned id, int timer)
@@ -62,6 +65,7 @@ int find(struct Page *page_table, int size, unsigned id, int timer)
     return res;
 }
 
+
 void simulate_lru(struct Page *page_table, int size, char *filename, int s)
 {
     unsigned addr;
@@ -75,10 +79,7 @@ void simulate_lru(struct Page *page_table, int size, char *filename, int s)
     int timer = 0;
     int lru = 0;
 
-    unsigned addr;
-    char rw;
-
-    while (fscanf(file, "%x %c ", &addr, &rw) == 2)
+    while (fscanf(file, "%x %c", &addr, &rw) == 2)
     {
         timer++;
 
@@ -186,7 +187,142 @@ void simulate_fifo(struct Page *page_table, int size, char *filename, int s)
     fclose(file);
 }
 
-int main(int argc, char* argv[])
+void simulate_2a(struct Page *page_table, int size, char *filename, int s)
+{
+    unsigned addr;
+    char rw;
+    int hit;
+
+    int used_adresses = 0;
+
+    FILE *file = fopen(filename, "r");
+
+    int timer = 0;
+
+    while (fscanf(file, "%x %c", &addr, &rw) == 2)
+    {
+        if (rw == 'R')
+        {
+            hit = find(page_table, size, id_calculator(addr, s), timer);
+
+            // Caso a página não esteja na memória, "Carregamos" ela do disco
+            if (!hit)
+            {
+                rec.page_faults++;
+
+                if (used_adresses < size)
+                {
+                    add(page_table, used_adresses, id_calculator(addr, s), timer);
+                    page_table[used_adresses].ref_bit = 1;
+                    used_adresses++;
+                }
+                else // Segunda Chance
+                {
+                    
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (page_table[i].ref_bit)
+                        {
+                            page_table[i].ref_bit = 0;
+                        }
+                        else
+                        {
+                            add(page_table, i, id_calculator(addr, s), timer);
+                            page_table[i].ref_bit = 1;
+                            break;
+                        }
+                    }
+
+                }
+            }
+        }
+        else
+        {
+            if (used_adresses < size)
+            {
+                add(page_table, used_adresses, id_calculator(addr, s), timer);
+                page_table[used_adresses].ref_bit = 1;
+
+                used_adresses++;
+            }
+            else
+            {
+                rec.dirty_writes++;
+                
+                for (int i = 0; i < size; i++)
+                {
+                    if (page_table[i].ref_bit)
+                    {
+                        page_table[i].ref_bit = 0;
+                    }
+                    else
+                    {
+                        add(page_table, i, id_calculator(addr, s), timer);
+                        page_table[i].ref_bit = 1;
+                        break;
+                    }
+                }
+            }
+        }
+    };
+
+    fclose(file);
+}
+
+void simulate_random(struct Page *page_table, int size, char *filename, int s)
+{
+    srand(time(NULL));
+    unsigned addr;
+    char rw;
+    int hit;
+
+    int used_adresses = 0;
+
+    FILE *file = fopen(filename, "r");
+
+    int timer = 0;
+
+    while (fscanf(file, "%x %c", &addr, &rw) == 2)
+    {
+        if (rw == 'R')
+        {
+            hit = find(page_table, size, id_calculator(addr, s), timer);
+            if (!hit)
+            {
+                rec.page_faults++;
+
+                if (used_adresses < size) // Se tem espaço vazio
+                {
+                    add(page_table, used_adresses, id_calculator(addr, s), timer);
+                    used_adresses++;
+                }
+                else // Random
+                {
+                    int r = rand() % size;
+                    add(page_table, r, id_calculator(addr, s), timer);
+                }
+            }
+        }
+        else
+        {
+            if (used_adresses < size) // Se tem espaço vazio
+            {
+                add(page_table, used_adresses, id_calculator(addr, s), timer);
+                used_adresses++;
+            }
+            else
+            {
+                rec.dirty_writes++;
+                int r = rand() % size;
+                add(page_table, r, id_calculator(addr, s), timer);
+            }
+        }
+    };
+
+    fclose(file);
+}
+
+int main(int argc, char *argv[])
 {
 
     // Parâmetros passados
@@ -217,6 +353,18 @@ int main(int argc, char* argv[])
     if (strcmp(alg, "lru") == 0)
     {
         simulate_lru(page_table, size, filename, s);
+    }
+    else if (strcmp(alg, "2a") == 0)
+    {
+        simulate_2a(page_table, size, filename, s);
+    }
+    else if (strcmp(alg, "fifo") == 0)
+    {
+        simulate_fifo(page_table, size, filename, s);
+    }
+    else if (strcmp(alg, "random") == 0)
+    {
+        simulate_random(page_table, size, filename, s);
     }
     else
     {
