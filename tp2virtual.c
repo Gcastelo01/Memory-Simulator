842a223c3dff9a3489e8fc5bb088c5f9400e48b9
Henrique Rotsen <henriquerotsensf@gmail.com>
2023-06-18 10:24:35 -0300

    resolvendo erros

--- tp2virtual.c
+++ tp2virtual.c
@@ -2,96 +2,68 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "recorder.h"
-#include "page.h"
-
-
-/**
- * @brief Procura um determinado endereço de memória na tabela de páginas. 
- * 
- * @param page_table Tabela de páginas
- * @param size Tamanho da tabela de páginas
- * @param id Endereço lógico de memória
- * @param timer Timer para atualizar page_table
- *
- * @return Bit indicador de hit (1 caso tenha encontrado, 0 caso não).
- */
-int find(Page *page_table[], int size, unsigned id, int timer)
+struct Page
 {
-    int res = 0;
-    for (int i = 0; i < size; i++)
-    {
-        if (id == page_table[i]->id)
-        {
-            res = 1;
-            page_table[i]->timer = timer;
-        }
-    }
-    return res;
+    int id;
+    int dirty;
+    int second_chance;
+    int timer;
+};
+
+struct Recorder
+{
+    char *configuracao[4];
+    unsigned page_faults;
+    unsigned dirty_writes;
+};
+
+struct Recorder rec;
+
+void print_results()
+{
+    printf("Executando o simulador... \n\tTécnica de reposição: %s\n\tArquivo de entrada: %s\n\tTamanho das Páginas: %sKB\n\tTamanho da memória usada: %sKB\n", rec.configuracao[0], rec.configuracao[1], rec.configuracao[2], rec.configuracao[3]);
+    printf("\tPage faults: %u\n", rec.page_faults);
+    printf("\tPáginas sujas escritas: %u", rec.dirty_writes);
 }
 
-/**
- *
- * @brief Adiciona um determiando endereço na tabela de páginas
- *
- * @param page_table Tabela de páginas
- * @param idx Índice de 
- *
- *
- */
-int add(Page *page_table[], int idx, unsigned addr, int timer)
+int size_calculator(int page_size)
 {
-    page_table[idx]->id = addr;
-    page_table[idx]->timer = timer;
-}
-
-/**
- * @brief Calcula o offset S das páginas, com base no tamanho.
- *
- * @param page_size Tamanho da página.
- *
- * @return S, o offset.
- */
-unsigned size_calculator(unsigned page_size)
-{
-    unsigned tmp, s;
-
-    s = 0;
-    tmp = page_size;
-
+    int tmp = page_size, s = 0;
     while (tmp > 1)
     {
         tmp = tmp >> 1;
         s++;
     }
-
-    return s;
+    return s + 10;
 };
 
-/**
- * @brief Calcula o índice da página na tabela de páginas.
- *
- * @param addr Endereço lógico
- * @param s Offset
- *
- * @return Índice da tabela de páginas
- */
 unsigned id_calculator(unsigned addr, int s)
 {
     return addr >> s;
 }
 
-/**
- * @brief Simula o funcionamento de uma memória virtual através do algoritmo LRU
- *
- * @param rec Ponteiro para o registrador de atividades
- * @param page_size Tamanho da página (em KB)
- * @param mem_size Tamanho da memória total disponível
- * @param filename Arquivo .log com todos os acessos de leitura e escrita da memória
- */
-void simulate_lru(struct Recorder *rec, Page page_table[], int size, char *filename, int s)
+void add(struct Page *page_table, int idx, unsigned addr, int timer)
 {
-    printf("1\n");
+    page_table[idx].id = addr;
+    page_table[idx].timer = timer;
+}
+
+int find(struct Page *page_table, int size, unsigned id, int timer)
+{
+    int res = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (id == page_table[i].id)
+        {
+            res = 1;
+            page_table[i].timer = timer;
+        }
+    }
+    return res;
+}
+
+void simulate_lru(struct Page *page_table, int size, char *filename, int s)
+{
     unsigned addr;
     char rw;
     int hit;
@@ -106,16 +78,17 @@ void simulate_lru(struct Recorder *rec, Page page_table[], int size, char *filen
     while (fscanf(file, "%x %c", &addr, &rw) == 2)
     {
         timer++;
-        printf("2\n");
-        if (strcmp("R", rw))
+
+        if (rw == 'R')
         {
-            hit = find(&page_table, size, id_calculator(addr, s), timer);
+            hit = find(page_table, size, id_calculator(addr, s), timer);
             if (!hit)
             {
-                printf("3\n");
-                if (used_adresses < size)// Se tem espaco vazio
+                rec.page_faults++;
+
+                if (used_adresses < size) // Se tem espaço vazio
                 {
-                    add(&page_table, used_adresses, id_calculator(addr, s), timer);
+                    add(page_table, used_adresses, id_calculator(addr, s), timer);
                     used_adresses++;
                 }
                 else // LRU
@@ -130,19 +103,21 @@ void simulate_lru(struct Recorder *rec, Page page_table[], int size, char *filen
                             aux = i;
                         }
                     }
-                    add(&page_table, aux, id_calculator(addr, s), timer);
+                    add(page_table, aux, id_calculator(addr, s), timer);
                 }
             }
         }
         else
         {
-            if (used_adresses < size) // Se tem espaco vazio
+            if (used_adresses < size) // Se tem espaço vazio
             {
-                add(&page_table, used_adresses, id_calculator(addr, s), timer);
+                add(page_table, used_adresses, id_calculator(addr, s), timer);
                 used_adresses++;
             }
             else // LRU
             {
+                rec.dirty_writes++; // Escrita de página suja
+
                 int aux;
                 lru = page_table[0].timer; // menos usado
                 for (int i = 1; i < size; i++)
@@ -153,137 +128,99 @@ void simulate_lru(struct Recorder *rec, Page page_table[], int size, char *filen
                         aux = i;
                     }
                 }
-                add(&page_table, aux, id_calculator(addr, s), timer);
+                add(page_table, aux, id_calculator(addr, s), timer);
             }
         }
     };
+
+    fclose(file);
 }
 
-/**
- * @brief Simula o funcionamento de uma memória virtual através do algoritmo Segunda Chance
- *
- * @param rec Ponteiro para o registrador de atividades
- * @param page_size Tamanho da página (em KB)
- * @param mem_size Tamanho da memória total disponível
- * @param filename Arquivo .log com todos os acessos de leitura e escrita da memória
- */
-void simulate_2a(struct Recorder *rec, Page page_table[], int size, char *filename, int s)
+void simulate_fifo(struct Page *page_table, int size, char *filename, int s)
 {
     unsigned addr;
     char rw;
+    int hit;
+
+    int used_adresses = 0;
 
     FILE *file = fopen(filename, "r");
 
-    while (fscanf(file, "%x %c", &addr, &rw) == 2)
-    {
-
-        if (strcmp("R", rw))
-        {
-        }
-        else
-        {
-        }
-    };
-}
-
-/**
- * @brief Simula o funcionamento de uma memória virtual através do algoritmo FIFO
- *
- * @param rec Ponteiro para o registrador de atividades
- * @param page_size Tamanho da página (em KB)
- * @param mem_size Tamanho da memória total disponível
- * @param filename Arquivo .log com todos os acessos de leitura e escrita da memória
- */
-void simulate_fifo(struct Recorder *rec, Page page_table[], int size, char *filename, int s)
-{
-    unsigned addr;
-    char rw;
-
-    FILE *file = fopen(filename, "r");
+    int timer = 0;
 
     while (fscanf(file, "%x %c", &addr, &rw) == 2)
     {
-
-        if (strcmp("R", rw))
+        if (rw == 'R')
         {
+            hit = find(page_table, size, id_calculator(addr, s), timer);
+            if (!hit)
+            {
+                rec.page_faults++;
+
+                if (used_adresses < size) // Se tem espaço vazio
+                {
+                    add(page_table, used_adresses, id_calculator(addr, s), timer);
+                    used_adresses++;
+                }
+                else // FIFO
+                {
+                }
+            }
         }
         else
         {
+            if (used_adresses < size) // Se tem espaço vazio
+            {
+                add(page_table, used_adresses, id_calculator(addr, s), timer);
+                used_adresses++;
+            }
+            else // FIFO
+            {
+            }
         }
     };
-}
 
-/**
- * @brief Simula o funcionamento de uma memória virtual através do algoritmo Random
- *
- * @param rec Ponteiro para o registrador de atividades
- * @param page_size Tamanho da página (em KB)
- * @param mem_size Tamanho da memória total disponível
- * @param filename Arquivo .log com todos os acessos de leitura e escrita da memória
- */
-void simulate_random(struct Recorder *rec, Page page_table[], int size, char *filename, int s)
-{
-    unsigned addr;
-    char rw;
-
-    FILE *file = fopen(filename, "r");
-
-    while (fscanf(file, "%x %c", &addr, &rw) == 2)
-    {
-
-        if (strcmp("R", rw))
-        {
-        }
-        else
-        {
-        }
-    };
+    fclose(file);
 }
 
 int main(int argc, char *argv[])
 {
-    /*Leitura e configuração*/
-    struct Recorder rec;
 
-
-    char *alg = argv[1]; 
+    // Parâmetros passados
+    char *alg = argv[1];
     char *filename = argv[2];
-    
-    unsigned page_size = atoi(argv[3]);
-    unsigned mem_size = atoi(argv[4]);
+    int page_size = atoi(argv[3]);
+    int mem_size = atoi(argv[4]);
 
-    printf("Parametros passados: %d \nLi parâmetros: \nAlgoritmo: %s \nArquivo: %s \nPage size: %d \nMem Size: %d", argc, alg, filename, page_size, mem_size);
-
+    // Definição da tabela e tamanhos
     int size = mem_size / page_size;
-    
-    Page page_table[size];
+    struct Page *page_table = (struct Page *)malloc(size * sizeof(struct Page));
+    if (page_table == NULL)
+    {
+        printf("Falha na alocação de memória!\n");
+        return 1;
+    }
 
-    const unsigned s = size_calculator(page_size);
+    int s = size_calculator(page_size);
 
-    set_config(&rec, argv);
+    // Configurando o recorder
+    rec.dirty_writes = 0;
+    rec.page_faults = 0;
+    rec.configuracao[0] = argv[1];
+    rec.configuracao[1] = argv[2];
+    rec.configuracao[2] = argv[3];
+    rec.configuracao[3] = argv[4];
 
-    /*switch de algoritmos*/
     if (strcmp(alg, "lru") == 0)
     {
-        simulate_lru(&rec, page_table, size, filename, s);
-    }
-    else if (strcmp(alg, "2a") == 0)
-    {
-        simulate_2a(&rec, page_table, size, filename, s);
-    }
-    else if (strcmp(alg, "fifo") == 0)
-    {
-        simulate_fifo(&rec, page_table, size, filename, s);
-    }
-    else if (strcmp(alg, "random") == 0)
-    {
-        simulate_random(&rec, page_table, size, filename, s);
+        simulate_lru(page_table, size, filename, s);
     }
     else
     {
-        printf("Erro, algoritmo inválido.");
+        printf("Erro, algoritmo inválido.\n");
+        return 1;
     }
+    print_results();
 
-    print_results(&rec);
     return 0;
 }
